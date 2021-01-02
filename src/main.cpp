df5c6bb5676411f042181a5523272bd93226a826
remico <remicollab@gmail.com>
2021-01-02 18:41:01 +0200

    add WDT system resetting (~8 sec)

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,55 @@
 #include <Arduino.h>
+#include <avr/wdt.h>
 #include <AsyncDelay.h>
-#include <GyverPower.h>
 #include "battery_util.h"
 #include "sbsproxy.h"
 
 sbs::SBSProxy *proxy = sbs::SBSProxy::instance();
 AsyncDelay readInterval;
 
-void goSleep()
+void WDT_setup()
 {
-
+    cli();
+    wdt_reset();
+    /* Clear WDRF in MCUSR */
+    MCUSR &= ~_BV(WDRF);
+    /* Start timed equence */
+    WDTCSR |= _BV(WDCE) | _BV(WDE);
+    /* prescaler = 1024K cycles (~8s), WD system reset mode */
+    WDTCSR = _BV(WDE) | _BV(WDP3) | _BV(WDP0);
+    sei();
 }
 
-void onWakeUp()
+void goSleep()
 {
+    /* force disable ADC and comparator */
+    ADCSRA &= ~_BV(ADEN); 			// ADC
+    ACSR |= _BV(ACD); 				// comparator
 
+    // IDLE sleep + sleep enable
+	SMCR = _BV(SE);
+
+    // disable BOD
+    // MCUCR = (0x03 << 5);
+    // MCUCR = (0x02 << 5);
+
+    // enter sleep mode
+    asm ("sleep");
+
+    // explicitly forbid sleeping on waking up
+    SMCR = 0;
+
+	/* re-enable ADC and comparator */
+	// if (!(PRR & _BV(PRADC))) {
+	// 	ADCSRA |= _BV(ADEN);
+	// 	ACSR &= ~ _BV(ACD);
+	// }
 }
 
 void setup()
 {
+    WDT_setup();
+
     Serial.begin(115200);
     // digitalWrite(0, HIGH);
     // digitalWrite(1, HIGH);
@@ -28,29 +59,27 @@ void setup()
     Serial.println(proxy->battery().address(), HEX);
 
     readInterval.start(5000, AsyncDelay::MILLIS);
-    _delay_ms(100);  // sanity
+    _delay_ms(100); // sanity
 
     // read the battery
     // Serial.println(F("\n++++++++++++ BATTERY ++++++++++++"));
     // readBattery(battery);
     // Serial.println(F("********************************************\n"));
 
-    _delay_ms(1000);  // sanity
-    proxy->enableBattery(true);  // make the battery proxy visible for the laptop
-
-    power.setSleepMode(IDLE_SLEEP);
+    _delay_ms(1000);            // sanity
+    proxy->enableBattery(true); // make the battery proxy visible for the laptop
 }
 
 void loop(void)
 {
     handleUserInput(proxy);
 
-    if (readInterval.isExpired())
-    {
+    if (readInterval.isExpired()) {
         printHumanizedBatteryStatus(proxy->status());
         proxy->printPowerStats();
         readInterval.restart();
     }
 
-    power.sleep(SLEEP_FOREVER);
+    goSleep();
+    wdt_reset();
 }
